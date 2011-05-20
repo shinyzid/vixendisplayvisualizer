89e2e7e5d6d24b6cf403d6aa128dda5dbbe1f056
Erik.Mathisen@gmail.com <Erik.Mathisen@gmail.com@5f42f882-ac2e-7008-18d7-9f30a8a0cf1a>
2011-05-20 00:53:16 +0000

    Added initial view model code, and some code organization.

---
+++ trunk/VixenDisplayVisualizer/ViewModels/SetupViewModel.cs
@@ -0,0 +1,13 @@
+// --------------------------------------------------------------------------------
+// Copyright (c) 2011 Erik Mathisen
+// See the file license.txt for copying permission.
+// --------------------------------------------------------------------------------
+namespace Vixen.PlugIns.VixenDisplayVisualizer.ViewModels
+{
+    /// <summary>
+    /// The setup view model.
+    /// </summary>
+    public class SetupViewModel
+    {
+    }
+}
