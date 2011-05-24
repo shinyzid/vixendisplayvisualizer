81772eb5161a65e2024651d7c872fa063e92ed3e
Erik.Mathisen <Erik.Mathisen@gmail.com>
2011-05-24 00:59:09 +0000

    Added the ElementEditor, with initial mock up.

---
+++ VixenDisplayVisualizer/Views/ElementEditorView.xaml.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) 2011 Erik Mathisen
+// See the file license.txt for copying permission.
+// --------------------------------------------------------------------------------
+namespace Vixen.PlugIns.VixenDisplayVisualizer.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ElementEditorView.xaml
+    /// </summary>
+    public partial class ElementEditorView : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ElementEditorView"/> class.
+        /// </summary>
+        public ElementEditorView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
