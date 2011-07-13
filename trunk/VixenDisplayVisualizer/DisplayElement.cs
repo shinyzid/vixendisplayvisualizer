089b8e08cd4ca29a81ff12faee101e0ae7b71562
erik@mathisen.us <erik@mathisen.us@5f42f882-ac2e-7008-18d7-9f30a8a0cf1a>
2011-07-13 00:53:34 +0000

    Hooked more of the logic together and started debugging the setup side.

--- trunk/VixenDisplayVisualizer/DisplayElement.cs
+++ trunk/VixenDisplayVisualizer/DisplayElement.cs
@@ -5,34 +5,144 @@
 namespace Vixen.PlugIns.VixenDisplayVisualizer
 {
     using System.Collections.Generic;
+    using System.ComponentModel;
 
     using Vixen.PlugIns.VixenDisplayVisualizer.Channels;
 
-    public class DisplayElement
+    public class DisplayElement : INotifyPropertyChanged
     {
-        public DisplayElement(int columns, int rows, int height, int leftOffset, int topOffset, int width, IList<MappedChannel> mappedChannels)
+        private int columns;
+
+        private int height;
+
+        private int leftOffset;
+
+        private string name;
+
+        private int rows;
+
+        private int topOffset;
+
+        private int width;
+
+        public DisplayElement(
+            int columns, 
+            int rows, 
+            int height, 
+            int leftOffset, 
+            int topOffset, 
+            int width, 
+            IList<MappedChannel> mappedChannels)
         {
-            Columns = columns;
-            Rows = rows;
-            Height = height;
-            LeftOffset = leftOffset;
-            TopOffset = topOffset;
-            Width = width;
-            MappedChannels = mappedChannels;
+            this.Columns = columns;
+            this.Rows = rows;
+            this.Height = height;
+            this.LeftOffset = leftOffset;
+            this.TopOffset = topOffset;
+            this.Width = width;
+            this.MappedChannels = mappedChannels;
         }
 
-        public int Columns { get; private set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public int Rows { get; private set; }
+        public int Columns
+        {
+            get
+            {
+                return this.columns;
+            }
 
-        public int Height { get; private set; }
+            set
+            {
+                this.columns = value;
+                this.PropertyChanged.NotifyPropertyChanged("Columns", this);
+            }
+        }
 
-        public int LeftOffset { get; private set; }
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
 
-        public int TopOffset { get; private set; }
+            set
+            {
+                this.height = value;
+                this.PropertyChanged.NotifyPropertyChanged("Height", this);
+            }
+        }
 
-        public int Width { get; private set; }
+        public int LeftOffset
+        {
+            get
+            {
+                return this.leftOffset;
+            }
+
+            set
+            {
+                this.leftOffset = value;
+                this.PropertyChanged.NotifyPropertyChanged("LeftOffset", this);
+            }
+        }
 
         public IList<MappedChannel> MappedChannels { get; private set; }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+                this.PropertyChanged.NotifyPropertyChanged("Name", this);
+            }
+        }
+
+        public int Rows
+        {
+            get
+            {
+                return this.rows;
+            }
+
+            set
+            {
+                this.rows = value;
+                this.PropertyChanged.NotifyPropertyChanged("Rows", this);
+            }
+        }
+
+        public int TopOffset
+        {
+            get
+            {
+                return this.topOffset;
+            }
+
+            set
+            {
+                this.topOffset = value;
+                this.PropertyChanged.NotifyPropertyChanged("TopOffset", this);
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                this.width = value;
+                this.PropertyChanged.NotifyPropertyChanged("Width", this);
+            }
+        }
     }
 }
